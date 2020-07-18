570c306c3c2c05d4c836ca980ba3130dd1bf4911
Liem Nguyen <nguyenthanhliem.dn.vn@gmail.com>
2020-07-18 17:20:47 +0700

    Initialize the project

---
+++ SmashClock/SmashClock/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmashClock
+//
+//  Copyright © 2020 ThienBao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
